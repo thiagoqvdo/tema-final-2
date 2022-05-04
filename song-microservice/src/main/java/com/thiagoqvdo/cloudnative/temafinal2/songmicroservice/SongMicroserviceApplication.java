7c07e64f83f5af93ecb2838fae2f7a5a75da3929
Thiago Quevedo <thiagoquevedodossantos@gmail.com>
2022-05-04 18:50:35 -0300

    Adding project base

---
+++ song-microservice/src/main/java/com/thiagoqvdo/cloudnative/temafinal2/songmicroservice/SongMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoqvdo.cloudnative.temafinal2.songmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongMicroserviceApplication.class, args);
+	}
+
+}
---
+++ song-microservice/src/test/java/com/thiagoqvdo/cloudnative/temafinal2/songmicroservice/SongMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoqvdo.cloudnative.temafinal2.songmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
